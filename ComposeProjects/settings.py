e845e3ccdb51158cbbb1e74a04282b6ea508b2b3
wlzkxm2 <wlzkxm2@gmail.com>
2024-01-17 00:45:45 +0900

    Update STATIC_ROOT and STATICFILES_DIRS paths

--- ComposeProjects/settings.py
+++ ComposeProjects/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, './ComposeProjects/statics')
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, './ComposeProjects/statics'),
+    os.path.join(BASE_DIR, 'static'),
     )
 
 # Default primary key field type
